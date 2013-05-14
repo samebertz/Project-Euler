e45557f773010232494e473a63c0b6d41f331eac
Sam Ebertz <samuel.ebertz@gmail.com>
2013-05-13 22:33:53 -0500

    finished problem 4
    invoked black magic on problem 4

--- p004.c
+++ p004.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 char ispalindrome(int n){
-	int i=0,j;
+	int i=0,j,k;
 	char *c=(char*)malloc(sizeof(char)*7);
 	snprintf(c,7,"%d",n);
-	while(*c!='\0'){
-		c++;
+	while(*(c+i)!='\0'){
 		i++;
 	}
-	c-=i;
 	j=i/2;
-	for(;i>j;i--,c++){
-		if(*c!=*(c+i)){
+	for(k=0,i--;i>=j;i--,k++){
+		if(*(c+k)!=*(c+i)){
 			return 0;
 		}
 	}
 	return 1;
 }
 int main() {
-	int i,j,max;
+	int i,j,t,max;
 	for(i=100;i<1000;i++){
 		for(j=i;j<1000;j++){
-			if(ispalindrome(i*j)){
-				max=i*j;
+			t=i*j;
+			if(t>max&&ispalindrome(t)){
+				max=t;
 			}
 		}
 	}
