ce0e3fa3d1cd6b8a69c8a3bb983e423b9a9271ad
Sam Ebertz <samuel.ebertz@gmail.com>
2013-11-03 23:19:06 -0600

    Finished problem 16, added .nfs files to .gitignore

--- p016.c
+++ p016.c
@@ -2,19 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 int main() {
-	int i,j,k,p,q,r,carry=0;
-	long long int n;
-	char *c=(char*)malloc(sizeof(char)*11);
-	memset((void *)c,0,11);
+	int i,j,k,p=320,q=0,r,carry=0;
+	long long int n=0;
+	char *c=(char*)malloc(sizeof(char)*p);
+	memset((void *)c,0,p);
 	*c=1;
-	for(i=0;i<10;i=*(c+i+1)>0?i+1:i){
+	for(i=0;i<p-1;i=*(c+i+1)>0?i+1:i){
 		carry=0;
 		for(j=0;j<i+2;j++){
 			r=*(c+j)*2+carry;
 			*(c+j)=r%10;
 			carry=r/10;
-			if(j==i+1){printf("c: ");for(k=10;k>-1;k--){printf("%d",*(c+k));}printf("\n");}
+			if(j==i+1){++q;}
+			//if(j==i+1){printf("n: %d\t",++q);}for(k=p-1;k>-1;k--){printf("%d",*(c+k));}printf("\n");}
 		}
-		if(i==6){break;} // Need to adjust stop point.
+		if(q==1000){break;}
 	}
+	for(k=p-1;k>-1;k--){n+=*(c+k);}
+	printf("n: %lld\n",n);
 }
